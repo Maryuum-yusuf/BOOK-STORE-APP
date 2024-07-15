50e57a89463bd1eed92521897bbe8aedadf61e75
Maryuum-yusuf <maryamayusuf690@gmail.com>
2024-07-15 17:40:47 +0300

    first

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,13 +4,10 @@ const About = () => {
   return (
     <section className="about">
       <h1>About Us</h1>
-      <p>We are an online book store</p>
-      <p>    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Perferendis aperiam fugiat ullam distinctio iusto sunt, numquam,
-            quis reiciendis pariatur error itaque? Voluptatem libero maiores
-            eveniet fugiat provident architecto minima pariatur! Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Aliquam perferendis
-            nemo hic laborum aspernatur.</p>
+      <p>We are book store</p>
+      <p>  Welcome to our book Store the ultimate solution for accessing and managing book store data. 
+        Our book Store provides a seamless and efficient way to integrate a
+         comprehensive  of books, authors,  carts of books  inventory into your applications.</p>
     
     </section>
   );
